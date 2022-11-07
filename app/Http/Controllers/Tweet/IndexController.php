03b5a5f7594688f4c3ce56064e7db1fa99c08249
ashiba <ashibasama@gmail.com>
2022-11-08 06:39:06 +0900

    seederコマンド完了

--- app/Http/Controllers/Tweet/IndexController.php
+++ app/Http/Controllers/Tweet/IndexController.php
@@ -7,14 +7,8 @@ use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
-    /**
-     * Handle the incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function __invoke(Request $request)
     {
-        //
+        return view("tweet.index", ["name" => "laravel"]);
     }
 }
---
+++ database/seeders/TweetsSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Tweet;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TweetsSeeder extends Seeder
+{
+    
+    public function run()
+    {
+        Tweet::create([
+            "content" => "－投稿１です！－",
+        ]);
+
+        Tweet::create([
+            "content" => "－投稿2です!－",
+        ]);
+    }
+}
