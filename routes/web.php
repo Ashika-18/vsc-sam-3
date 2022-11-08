4b54ef4fa3d8e97122d0e7e2022086817d396f63
ashiba <ashibasama@gmail.com>
2022-11-08 20:27:17 +0900

    次は削除処理

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,6 @@ Route::get("/tweet", \App\Http\Controllers\Tweet\IndexController::class)->name("
 
 Route::post("/tweet/create", App\Http\Controllers\Tweet\CreateController::class)->name("tweet.create");
 
-Route::get("/tweet/update/{tweetId}", \App\Http\Controllers\Tweet\Update\IndexController::class)->name("tweet.update.index");
+Route::get("/tweet/update/{tweetId}", \App\Http\Controllers\Tweet\Update\IndexController::class)->name("tweet.update.index")->wehre("tweetId", "[0-9]+");
 
-Route::put("/tweet/update/{tweetId}", \App\Http\Controllers\Tweet\Update\PutController::class)->name("tweet.update.put");
+Route::put("/tweet/update/{tweetId}", \App\Http\Controllers\Tweet\Update\PutController::class)->name("tweet.update.put")->wehre("tweetId", "[0-9]+");
